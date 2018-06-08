57e768ce3f93c4172d1a16549415dc0c029b5225
pyx26266 <amankumar.msjps@gmail.com>
2018-06-08 06:27:49 +0000

    faker added for databse filling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create(name: Faker::Name.name, email: Faker::Internet.email, 
+              password: "pa55w0rd", password_confirmation: 'pa55w0rd')
+end
